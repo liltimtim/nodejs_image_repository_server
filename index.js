f4115727a8c2cbce88a192d502ec81f01ebc1fbd
Timothy Dillman <timothy.dillman@mobilefirst.me>
2022-12-24 13:37:41 -0600

    added collection name for client usage

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.get("/weathercollections", async (req, res) => {
     dirs = await fs.readdir(`${FILE_PATH}/${foundCollection}`, {
       withFileTypes: true,
     });
-    return res.json({ result: dirs });
+    return res.json({ result: dirs, collection: foundCollection });
   } catch (err) {
     logError(err);
     res.status(404).json({ error: err.message });
